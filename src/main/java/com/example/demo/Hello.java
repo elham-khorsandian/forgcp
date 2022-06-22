b3ff67cb1eab61f217f8535b0de53fdf22659089
Eli Kh <elikh@Elis-MBP.fritz.box>
2022-06-22 12:44:01 +0200

    my commit

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Hello {
+    @Id
+    private int id;
+
+    private String greeting;
+    public Hello(){
+
+    }
+}
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@CrossOrigin
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloRepository repo;
+
+    @GetMapping("/hello")
+    public List<Hello> sayHello() {return repo.findAll();}
+
+
+}
